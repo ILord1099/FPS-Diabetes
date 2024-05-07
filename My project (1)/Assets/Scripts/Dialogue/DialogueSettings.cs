1b0d177fdd3f4f687ea491a0958619b83df90471
Carlos dos anjos <Carlinhos.anjos99@gmail.com>
2024-05-07 15:53:47 -0300

    teste video

---
+++ My project (1)/Assets/Scripts/Dialogue/DialogueSettings.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName =  "New Dialogue", menuName ="New dialogue/ Dialogue")] 
+public class DialogueSettings : ScriptableObject
+{
+   
+}
