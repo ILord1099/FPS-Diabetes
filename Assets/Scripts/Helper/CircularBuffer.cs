6530bea8bf1820261f7d320116ccd20c311a47e1
Mikael Cavalcanti <mcs11@cin.ufpe.br>
2024-05-23 11:10:08 -0300

    add buffer do controller do botão

---
+++ Assets/Scripts/Helper/CircularBuffer.cs
@@ -0,0 +1,88 @@
+namespace Helper
+{
+    [System.Serializable]
+    public class CircularBuffer<T>
+    {
+        private T[] _buffer;
+        private int _front, _back, _capacity;
+
+        private bool IsEmpty => Count == 0;
+
+        private bool IsFull => Count == _capacity;
+
+        public int Count { get; private set; }
+
+        public CircularBuffer(int capacity)
+        {
+            _capacity = capacity;
+            Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            if (_buffer[_back] is null) return false;
+
+            for (var i = 0; i < _buffer.Length; i++)
+            {
+                if (_buffer[i].Equals(item))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void Enqueue(T item)
+        {
+            if (IsFull || Contains(item)) return;
+
+            Count++;
+            _buffer[_back] = item;
+            _back = (_back + 1) % _capacity;
+        }
+
+        public T Dequeue()
+        {
+            if (IsEmpty) return default;
+
+            Count--;
+            var item = _buffer[_front];
+            _front = (_front + 1) % _capacity;
+            return item;
+        }
+
+        public bool TryDequeue(out T item)
+        {
+            if (IsEmpty)
+            {
+                item = default;
+                return false;
+            }
+
+            item = Dequeue();
+            return true;
+        }
+
+        public T Peek() => IsEmpty ? default : _buffer[_front];
+
+        public bool TryPeek(out T item)
+        {
+            if (IsEmpty)
+            {
+                item = default;
+                return false;
+            }
+
+            item = Peek();
+            return true;
+        }
+
+        public void Clear()
+        {
+            _buffer = new T[_capacity];
+
+            Count = _front = _back = 0;
+        }
+    }
+}
