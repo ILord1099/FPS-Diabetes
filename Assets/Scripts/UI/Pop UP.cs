b7172d462074e98a903754d03badf1e991d29a66
ArthurC01 <j.arthur.cavalcanti@gmail.com>
2024-06-04 11:49:16 -0300

    modal links externos

--- Assets/Scripts/UI/Pop UP.cs
+++ Assets/Scripts/UI/Pop UP.cs
@@ -6,9 +6,10 @@ public class PopUpManager : MonoBehaviour
     public Transform popUpTransform; // Referência ao Transform do pop-up
     public Button triggerButton; // Referência ao botão que aciona o pop-up
     public float duration = 0.5f; // Duração da animação
+    public Image imageToControl;
 
     private Vector3 hiddenScale = new Vector3(0, 0, 0); // Escala inicial do pop-up (escondido)
-    private Vector3 shownScale = new Vector3(12, 8, 0); // Escala final do pop-up (visível)
+    private Vector3 shownScale = new Vector3(12,8 , 0); // Escala final do pop-up (visível)
 
     void Start()
     {
@@ -24,6 +25,7 @@ public class PopUpManager : MonoBehaviour
     {
         // Anima o pop-up para a escala visível
         popUpTransform.DOScale(shownScale, duration).SetEase(Ease.OutBack);
+        DisableImage();
     }
 
     // Opcional: Método para esconder o pop-up
@@ -31,8 +33,25 @@ public class PopUpManager : MonoBehaviour
     {
         // Anima o pop-up de volta para a escala escondida
         popUpTransform.DOScale(hiddenScale, duration).SetEase(Ease.InBack);
+        EnableImage();
     }
 
+    // Função para desativar a imagem
+    public void DisableImage()
+    {
+        if (imageToControl != null)
+        {
+            imageToControl.enabled = false;
+        }
+    }
 
+    // Função para ativar a imagem
+    public void EnableImage()
+    {
+        if (imageToControl != null)
+        {
+            imageToControl.enabled = true;
+        }
+    }
 }
 
