3f1338c3b03eb57ae9df3ea7ac5da7bb1b8a26d6
ArthurC01 <j.arthur.cavalcanti@gmail.com>
2024-06-13 10:54:34 -0300

    Logo Collect

--- Assets/Scripts/PlatformScripts/LogoScript.cs
+++ Assets/Scripts/PlatformScripts/LogoScript.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 
 public class LogoScript : MonoBehaviour
 {
-     void Start()
+    private Tween movementTween;
+    private Vector3 originalPosition;
+    void Start()
     {
-        transform.DOLocalMoveY(-1f, 1f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
+        originalPosition = transform.position;
+        movementTween = transform.DOLocalMoveY(originalPosition.y -0.5f, 1f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
     }
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -21,6 +24,20 @@ public class LogoScript : MonoBehaviour
     void Collect()
     {
        
-        Destroy(gameObject);
+        Destroy(gameObject);  
+        OnDestroy();
+    }
+
+    public Vector3 GetOriginalPosition()
+    {
+        return originalPosition;
+    }
+    void OnDestroy()
+    {
+        // Certificar-se de matar o tween ao destruir o objeto para evitar erros
+        if (movementTween != null)
+        {
+            movementTween.Kill();
+        }
     }
 }
